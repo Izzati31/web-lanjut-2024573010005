efa305a26369efb32b8cc6207d3983564d0aa066
Izzati Nurvira <izzatinurvira3126@gmail.com>
2025-10-09 21:27:29 +0700

    Update Folder

---
+++ project/calculator/resources/views/mywelcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Welcome to Laravel</title>
+    </head>
+<body>
+    <h1>Welcome to Laravel!</h1>
+    <p>
+        Hello, {{$message}}
+    </p>
+</body>
+</html>
